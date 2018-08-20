dc20a7efa10ce75df0df5ebb037e4a48b65b4e3d
Suresh <sureshg1986@gmail.com>
2018-08-20 10:10:06 -0500

    Add files via upload

---
+++ src/main/java/com/shopping/online/shoppingcart/controller/ShoppingCartController.java
@@ -0,0 +1,31 @@
+package com.shopping.online.shoppingcart.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShoppingCartController {
+
+	@RequestMapping
+	public Cart getCart(Cart cart){
+		
+		
+		
+	}
+	
+	public Cart addProductToCart(Cart cart){
+		
+	}
+	
+	
+	public Cart editProductInCart(Cart cart){
+		
+	}
+	
+	public Cart deleteProductFromCart(Cart cart){
+		
+		
+	}
+	
+	
+}
